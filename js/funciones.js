baa38bfc9bbb8d17b4caeb21e5666ca1c27e9ab2
SandraCarretero <sandracarretero24@gmail.com>
2022-10-03 14:02:04 +0200

    jquey para horizontal

--- js/funciones.js
+++ js/funciones.js
@@ -1,28 +1,32 @@
 $(function(){
-    $(window).scroll( function(){
-        var scroll = $(window).scrollTop();
-        // console.log("scroll"+scroll);
+
+    if ($(window).width() >= 768) {
         
-        var distancia_elemento = $(".porfolio").offset().top;
-        // console.log("distancia elemento"+distancia_elemento);
+        $(window).scroll( function(){
+            var scroll = $(window).scrollTop();
+            // console.log("scroll"+scroll);
+            
+            var distancia_elemento = $(".porfolio").offset().top;
+            // console.log("distancia elemento"+distancia_elemento);
 
-        var max = $(window).width();
+            var max = $(window).width();
 
-        var move = distancia_elemento - scroll;
+            var move = distancia_elemento - scroll;
 
-        if(scroll >= distancia_elemento && scroll < distancia_elemento + 2*max ){
-            console.log("Estoy aquí")
-            $(".line").css({
-                "transform": "translateX("+move+"px)"
-            });
-        }
+            if(scroll >= distancia_elemento && scroll < distancia_elemento + 2*max ){
+                console.log("Estoy aquí")
+                $(".line").css({
+                    "transform": "translateX("+move+"px)"
+                });
+            }
 
-        else{
-            $(".line").css({
-                "transform": "translateX(0px)"
-            });
-        }
-    });
+            else{
+                $(".line").css({
+                    "transform": "translateX(0px)"
+                });
+            }
+        });
+    };
 
 
 
